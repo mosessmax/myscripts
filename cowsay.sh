d6acb6089a6e581a56850369dc64acdbce02fed9
adebayo-moses <adebayomoses597@gmail.com>
2023-12-07 00:46:59 +0100

    it got compliated real fast

--- cowsay.sh
+++ cowsay.sh
@@ -4,3 +4,23 @@ colors=($tput setaf 1 $tput setaf 2 $tput setaf 3 $tput setaf 4 $tput setaf 5 $t
 
 # random silly message
 echo "Hello, $USER. I hope you are doing well." | cowsay -f $(ls /usr/share/cowsay/cows/ | shuf -n1) | lolcat -F 0.5
+
+#random message 2
+messages=('Woo hoo, I'm a rainbow dog" "I'm colorful and I know it!" "So many colors, mooo!" "I'm a cow. *Rainbow explosion*" "Sometimes I pretend to be a unicorn 🦄"))
+
+message=${messages[$(($RANDOM % ${#messages[@]}))]}
+
+
+# print message in rainbow color 
+
+for ((i=0; i<${#message}; i++)); do
+    echo -n "${colors[$((RANDOM % ${#colors[@]}))]}${message:$i:1}"
+done
+
+#reset the color
+echo -e "$(tput sgr0)"
+
+#now onto cowsay
+
+echo "$message" | cowsay -f $(ls /usr/share/cowsay/cows | shuf -n1)
+
